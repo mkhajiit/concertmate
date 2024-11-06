82dfe1d5fdb6752118248505c58effc53a87ce25
mkhajiit <mkhajiit15@gmail.com>
2024-11-06 23:32:13 +0900

    api 데이터 확인작업

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -1,3 +1,4 @@
+// fetch함수 있는 파일
 import { NextResponse } from 'next/server';
 import xml2js from 'xml2js';
 
@@ -6,11 +7,12 @@ export async function GET() {
 
   try {
     const response = await fetch(
-      `http://www.kopis.or.kr/openApi/restful/pblprfr?service=${apiKey}&stdate=20230601&eddate=20230630&cpage=1&rows=10&prfstate=02&signgucode=11&signgucodesub=1111&kidstate=Y`
+      `http://www.kopis.or.kr/openApi/restful/pblprfr?service=${apiKey}&stdate=20241006&eddate=20241230&cpage=1&rows=10&shcate=DDDD`
     );
     const textData = await response.text();
     const data = await xml2js.parseStringPromise(textData);
-    return NextResponse.json(data.dbs.db);
+    const dbData = data?.dbs?.db || []; // 데이터가 없으면 빈 배열을 반환
+    return NextResponse.json(dbData);
   } catch (error) {
     console.log('Error from Fetching Data: ', error);
   }
