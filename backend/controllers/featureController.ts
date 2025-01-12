ced54bd4fafee1bc10f810b34027ac24f2c9e5e0
mkhajiit <mkhajiit15@gmail.com>
2025-01-12 17:32:16 +0900

    토큰 검증 로직에 주석 추가

--- backend/controllers/featureController.ts
+++ backend/controllers/featureController.ts
@@ -13,15 +13,18 @@ export async function verifyTokenController(req: Request, res: Response): Promis
     const accessToken = authHeader?.split(' ')[1];
     const refreshToken = req.cookies.refreshToken;
 
+    // 환경변수에 키가 지정되어있지 않은 경우
     if (!accessTokenKey || !refreshTokenKey) {
       throw new Error('환경변수에 알맞은 변수가 없습니다');
     }
 
+    // 로그인 하지 않은 경우
     if (!accessToken || !refreshToken) {
       res.status(401).json({ message: '토큰이 없습니다. 인증이 필요합니다.' });
       return;
     }
 
+    // 리프래시 토큰이 만료되면 로그인 하도록 유도하기위해 리프래시 토큰 만료를 먼저 검사함
     jwt.verify(
       refreshToken,
       refreshTokenKey,
@@ -31,8 +34,10 @@ export async function verifyTokenController(req: Request, res: Response): Promis
           return;
         }
 
+        // 리프래시 토큰에 저장된 페이로드
         const userId = decoded?.id;
 
+        // 엑세스 토큰이 만료 되었으면 재발급하고 만료되지 않았으면 유효인증
         jwt.verify(accessToken, accessTokenKey, (err) => {
           if (err) {
             const accessToken = jwt.sign({ id: userId }, accessTokenKey, {
